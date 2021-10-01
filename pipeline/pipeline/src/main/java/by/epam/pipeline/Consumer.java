7d6ab3f7c93cd4b86fb6ed85c7240b38719bd62e
Yauheni_Dzehtsiarou <Degtyarov96@mail.ru>
2021-10-01 15:03:33 +0300

    Write from kafka to hdfs

--- pipeline/pipeline/src/main/java/by/epam/pipeline/Consumer.java
+++ pipeline/pipeline/src/main/java/by/epam/pipeline/Consumer.java
@@ -1,12 +1,22 @@
 package by.epam.pipeline;
 
+import org.apache.hadoop.fs.FSDataOutputStream;
+import org.apache.hadoop.fs.FileSystem;
+import org.apache.hadoop.fs.Path;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cloud.stream.annotation.EnableBinding;
 import org.springframework.cloud.stream.annotation.StreamListener;
 import org.springframework.cloud.stream.messaging.Sink;
 import org.springframework.messaging.handler.annotation.SendTo;
 
+import java.io.IOException;
+
 @EnableBinding({Sink.class, KafkaProcessor.class})
 public class Consumer {
+
+    @Autowired
+    private FileSystem hdfs;
 
     @StreamListener(Sink.INPUT)
     @SendTo(KafkaProcessor.OUTPUT)
@@ -16,7 +26,32 @@ public class Consumer {
     }
 
     @StreamListener(KafkaProcessor.INPUT)
-    public void logKafka(String str) {
+    public void streamFromKafkaToHdfs(String str) {
         System.out.println("Received from kafka: " + str);
+        try {
+            writeToHdfs(str);
+        } catch (IOException e) {
+            System.out.println("Error while writing to HDFS");
+        }
+    }
+
+    @Value("${spring.hdfs.directory}")
+    private String DIR_NAME;
+    @Value("${spring.hdfs.filename}")
+    private String FILE_NAME;
+
+    private void writeToHdfs(String str) throws IOException {
+        Path dir = new Path(Path.SEPARATOR + DIR_NAME);
+        Path file = new Path(Path.SEPARATOR + DIR_NAME + Path.SEPARATOR + FILE_NAME);
+        if (!hdfs.exists(dir)) {
+            System.out.println("Creating dir and file");
+            hdfs.mkdirs(dir);
+            hdfs.create(file).close();
+        }
+
+        FSDataOutputStream output = hdfs.append(file);
+        output.writeUTF(str);
+        output.flush();
+        output.close();
     }
 }
--- pipeline/pipeline/src/main/java/by/epam/pipeline/PipelineApplication.java
+++ pipeline/pipeline/src/main/java/by/epam/pipeline/PipelineApplication.java
@@ -1,12 +1,37 @@
 package by.epam.pipeline;
 
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.FileSystem;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import java.io.IOException;
 
 @SpringBootApplication
 public class PipelineApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(PipelineApplication.class, args);
+    }
+
+    @Value("${spring.hdfs.url}")
+    private String HDFS_URL;
+
+    @Bean
+    public FileSystem hdfs() {
+        Configuration configuration = new Configuration();
+        configuration.set("fs.defaultFS", HDFS_URL);
+        configuration.set("dfs.client.block.write.replace-datanode-on-failure.enable", "true");
+        configuration.set("dfs.client.block.write.replace-datanode-on-failure.policy", "ALWAYS");
+        configuration.set("dfs.client.block.write.replace-datanode-on-failure.best-effort", "true");
+
+        try {
+            return FileSystem.get(configuration);
+        } catch (IOException e) {
+            System.err.println("Error while create HDFS bean: " + e);
+            return null;
+        }
     }
 }
